ff7ab55b8cafabc28c556a5aceb2b6782113efb7
heejin-pro <hjin.pro@gmail.com>
2021-10-14 23:58:27 +0900

    Add auth decorators

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm, SampleForm
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
@@ -18,6 +19,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -30,7 +32,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -48,6 +50,7 @@ def post_test(request):
     form = SampleForm()
     return render(request, 'blog/post_test.html', {'form': form})
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
@@ -57,6 +60,7 @@ def post_publish(request, pk):
     post.publish()
     return redirect('post_detail', pk=pk)
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
